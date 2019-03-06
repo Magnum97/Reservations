48e16033d400a5d03cfe2dca14f8e118f98138f6
Magnum <magnum1997@gmail.com>
2019-03-06 00:53:55 -0800

    Reservation make and remove.

--- src/main/java/me/magnum/reservations/commands/Reservation.java
+++ src/main/java/me/magnum/reservations/commands/Reservation.java
@@ -2,6 +2,9 @@ package me.magnum.reservations.commands;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Description;
+import co.aikar.commands.annotation.Subcommand;
 import me.magnum.reservations.util.SimpleConfig;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
@@ -9,13 +12,15 @@ import org.bukkit.command.CommandSender;
 
 import static me.magnum.reservations.util.Config.*;
 
+@CommandAlias("%command")
 public class Reservation extends BaseCommand {
 	
 	
 	private SimpleConfig data = new SimpleConfig("reservations.yml", false);
 	
 	
-	@CommandAlias("%command")
+	@Subcommand("make")
+	@Description("Get a number")
 	public void reservation (CommandSender sender, String player) {
 		String uuid;
 		OfflinePlayer p = Bukkit.getOfflinePlayer(player);
@@ -29,8 +34,31 @@ public class Reservation extends BaseCommand {
 		}
 		else {
 			sender.sendMessage(player + " has never logged in.");
-			return;
 		}
 	}
 	
+	@Subcommand("view")
+	@Description("View current reservations")
+	@CommandPermission("%command.view")
+	public void onView (CommandSender sender) {
+	
+	}
+	
+	@Subcommand("clear")
+	@Description("Clear a reservation from the list")
+	@CommandPermission("%command.clear")
+	public void onClear (CommandSender sender, String player) {
+		String uuid;
+		OfflinePlayer p = Bukkit.getOfflinePlayer(player);
+		uuid = p.getUniqueId().toString();
+		if (data.getString(uuid) != null) {
+			uuid = p.getUniqueId().toString();
+			data.set(uuid, null);
+			data.saveConfig();
+			sender.sendMessage(pre + player + " has been removed from the queue");
+		}
+		else {
+			sender.sendMessage(pre + player + " was not found in the queue");
+		}
+	}
 }
