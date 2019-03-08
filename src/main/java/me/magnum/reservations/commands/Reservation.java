0dfd86a37032c91cbe4a8eb3267a0c7aa733152d
Magnum <magnum1997@gmail.com>
2019-03-08 13:56:57 -0800

    Make reservations, save to config, view reservations and clear reservations.

--- src/main/java/me/magnum/reservations/commands/Reservation.java
+++ src/main/java/me/magnum/reservations/commands/Reservation.java
@@ -1,64 +1,56 @@
 package me.magnum.reservations.commands;
 
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Description;
-import co.aikar.commands.annotation.Subcommand;
-import me.magnum.reservations.util.SimpleConfig;
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
+import co.aikar.commands.CommandHelp;
+import co.aikar.commands.annotation.*;
+import me.magnum.lib.CheckSender;
+import me.magnum.reservations.util.DataWorks;
 import org.bukkit.command.CommandSender;
 
-import static me.magnum.reservations.util.Config.*;
+import java.util.LinkedHashMap;
+
+import static me.magnum.reservations.util.Config.pre;
 
 @CommandAlias("%command")
 public class Reservation extends BaseCommand {
 	
 	
-	private SimpleConfig data = new SimpleConfig("reservations.yml", false);
+	public Reservation () {
+	}
 	
 	
 	@Subcommand("make")
 	@Description("Get a number")
-	public void reservation (CommandSender sender, String player) {
-		String uuid;
-		OfflinePlayer p = Bukkit.getOfflinePlayer(player);
-		if (p.hasPlayedBefore()) {
-			uuid = p.getUniqueId().toString();
-			next++;
-			data.set("next-appointment", next);
-			data.set(uuid, next);
-			data.saveConfig();
-			sender.sendMessage(pre + player + confirmAppt);
-		}
-		else {
-			sender.sendMessage(player + " has never logged in.");
-		}
+	public void onMake (CommandSender sender, String player) {
+		DataWorks dw = new DataWorks();
+		String result = dw.make(player);
+		sender.sendMessage(pre + result);
 	}
 	
 	@Subcommand("view")
 	@Description("View current reservations")
-	@CommandPermission("%command.view")
+	@CommandPermission("appointment.view")
 	public void onView (CommandSender sender) {
-	
+		if (CheckSender.isCommand(sender)) {
+			return;
+		}
+		DataWorks dw = new DataWorks();
+		LinkedHashMap <Integer, String> result;
+		dw.view(sender);
 	}
+	
 	
 	@Subcommand("clear")
 	@Description("Clear a reservation from the list")
-	@CommandPermission("%command.clear")
-	public void onClear (CommandSender sender, String player) {
-		String uuid;
-		OfflinePlayer p = Bukkit.getOfflinePlayer(player);
-		uuid = p.getUniqueId().toString();
-		if (data.getString(uuid) != null) {
-			uuid = p.getUniqueId().toString();
-			data.set(uuid, null);
-			data.saveConfig();
-			sender.sendMessage(pre + player + " has been removed from the queue");
-		}
-		else {
-			sender.sendMessage(pre + player + " was not found in the queue");
-		}
+	@CommandPermission("appointment.clear")
+	public void onClear (CommandSender sender, int key) {
+		DataWorks dw = new DataWorks();
+		String result = dw.clear(key);
+		sender.sendMessage(pre + result);
+	}
+	
+	@HelpCommand
+	public void onHelp (CommandSender sender, CommandHelp help) {
+		help.showHelp();
 	}
 }
